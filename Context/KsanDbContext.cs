411d0069d382b9b9e97ebdf5de8ce6f4609ae1dd
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2021-09-16 16:56:25 +0200

    Project Created with Models Context and DataAnnotations

---
+++ Context/KsanDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Ksandswich.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ksandswich.Context
+{
+    public class KsanDbContext : DbContext
+    {
+        public KsanDbContext (DbContextOptions<KsanDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+
+    }
+}
---
+++ Models/Lanche.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ksandswich.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        [StringLength(100)]
+        public string Nome { get; set; }
+        [StringLength(100)]
+        public string DescricaoCurta { get; set; }
+        [StringLength(250)]
+        public string DescricaoDetalhada { get; set; }
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Preco { get; set; }
+        [StringLength(200)]
+        public string ImagemUrl { get; set; }
+        [StringLength(200)]
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
